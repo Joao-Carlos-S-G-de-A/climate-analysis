5c2acff7b69a59ab7e2bbfe7ac81ac1b4a99c16a
Joao <joaocsgalmeida@gmail.com>
2025-09-30 15:59:46 +0100

    Added rain conversion

---
+++ rainfall_conversion.py
@@ -0,0 +1,11 @@
+# import rain
+
+""" You are god"""
+
+def inches_to_mm(inches):
+	"""
+	convertd rainfall to metric
+	@param inches: Rainfall in inches
+	@returns: Rain in mm
+	"""
+	return inches*25.4
